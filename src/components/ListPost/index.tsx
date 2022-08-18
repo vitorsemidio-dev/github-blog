d4e30d6ad72d53b534eba5ac13edf025986e0169
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-08-18 05:32:22 -0300

    feat: Busca issues pelo search form

--- src/components/ListPost/index.tsx
+++ src/components/ListPost/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { api } from '../../services/api'
+import { useContext, useMemo } from 'react'
+import { PostsContext } from '../../context/PostsContext'
 import { formatterTimeAgo } from '../../utils/timeago'
 
 import { Text, Title } from '../Typography'
@@ -11,40 +11,14 @@ import {
   ListPostContainer,
 } from './styles'
 
-interface IPost {
-  id: number
-  body: string
-  html_url: string
-  title: string
-  created_at: string
-  number: number
-}
-
-interface ISearchIssueResponse {
-  items: IPost[]
-}
-
 export function ListPost() {
-  const q = 'Boas'
-  const username = 'rocketseat-education'
-  const repo = 'reactjs-github-blog-challenge'
-  const [posts, setPosts] = useState<IPost[]>([])
-
-  useEffect(() => {
-    const loadPost = async () => {
-      const response = await api.get<ISearchIssueResponse>('/search/issues', {
-        params: { q: `${q} repo:${username}/${repo}` },
-      })
-      setPosts(
-        response.data.items.map((item) => ({
-          ...item,
-          created_at: formatterTimeAgo(item.created_at),
-        })),
-      )
-    }
-
-    loadPost()
-  }, [])
+  const { posts: postsSource } = useContext(PostsContext)
+  const posts = useMemo(() => {
+    return postsSource.map((postSourceItem) => ({
+      ...postSourceItem,
+      created_at: formatterTimeAgo(postSourceItem.created_at),
+    }))
+  }, [postsSource])
 
   return (
     <ListPostContainer>
--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,3 +1,6 @@
+import { ChangeEvent, useContext, useEffect, useState } from 'react'
+import { PostsContext } from '../../context/PostsContext'
+import { useDebounce } from '../../hooks/useDebounce'
 import { Text, Title } from '../Typography'
 import {
   SearchFormContainer,
@@ -6,6 +9,20 @@ import {
 } from './styles'
 
 export function SearchForm() {
+  const { searchPosts } = useContext(PostsContext)
+
+  const [searchText, setSearchText] = useState<string>('')
+  const debouncedValue = useDebounce<string>(searchText, 5000)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value)
+  }
+
+  useEffect(() => {
+    searchPosts(searchText)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue])
+
   return (
     <SearchFormContainer>
       <SearchFormHeader>
@@ -15,7 +32,11 @@ export function SearchForm() {
         </Text>
       </SearchFormHeader>
       <SearchFormWrapper>
-        <input placeholder="Buscar conteúdo" />
+        <input
+          placeholder="Buscar conteúdo"
+          value={searchText}
+          onChange={handleChange}
+        />
       </SearchFormWrapper>
     </SearchFormContainer>
   )
